d8181aced28ee58b73747ea671f68c6d315ae7e4
Aedanos <santri0303@gmail.com>
2021-11-28 20:46:24 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import math
+import random
+a=list(range(100))
+print(len(str(2**1000)))
+print(math.pi)
+print(random.random())
+b="Spam"
+print(b[2])
+print(b[-2])
+print(b[1:3])
+dir(b)
+inv=["Меч", "Булава", "Топор", "Коса"]
+inv.append("Яблоко")
+print(inv)
+#print(random.random(["Меч", "Булава", "Топор", "Коса"]))
